62ec017c1e3337a39718ab73a76ca047f3c54104
YU <peixy1324@gmail.com>
2023-02-16 14:45:12 -0500

    Fix the bug when a todo typed in the input box will not reset. Fix the bug when have duplicate todo, there is no reminder.

--- web.py
+++ web.py
@@ -1,6 +1,9 @@
 import os
+import time
 
 import streamlit as st
+from streamlit.errors import DuplicateWidgetID
+
 import functions
 
 if not os.path.exists("todos.txt"):
@@ -14,16 +17,27 @@ def add_todo():
     todo_local = st.session_state['new_todo'] + "\n"
     todos.append(todo_local)
     functions.write_todos(todos)
+    st.session_state['new_todo'] = ""
 
-# def complete_todo():
 
 # The order of the code is important
 st.title("My Todo App")
 st.subheader("This is my todo app")
 st.write("This app is to increase your productivity")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    try:
+        checkbox = st.checkbox(todo, key=todo)
+        if checkbox:
+            todos.pop(index)
+            functions.write_todos(todos)
+            del st.session_state[todo]
+            st.experimental_rerun()
+    except DuplicateWidgetID:
+        todos.pop(index)
+        functions.write_todos(todos)
+
+        st.warning("You already have this todo, please type in a new one")
 
 st.text_input(label=" ", placeholder="Add new todo...",
               on_change=add_todo, key='new_todo')
